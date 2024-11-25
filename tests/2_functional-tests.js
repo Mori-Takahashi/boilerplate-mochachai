4c734d2ba50e88a0e7c9eef55e31aa3fe7db0fc4
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-11-25 15:30:27 +0000

    change #5

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -95,8 +95,8 @@ suite('Functional Tests with Zombie.js', function () {
           browser.fill('surname', 'Polo').then(() => {
               browser.pressButton('submit', () => {
                   browser.assert.success();
-                  browser.assert.text('span#name', 'Marco');
-                  browser.assert.text('span#surname', 'Polo');
+                  browser.assert.text('span#name', 'Cristoforo');
+                  browser.assert.text('span#surname', 'Colombo');
                   browser.assert.elements('span#dates', 1);
                   done();
               });
